ff72b67d0c0defd375b2337450dfde1111fe3211
Pedro Monroy <monroy.pedroa07@gmail.com>
2025-08-07 09:48:59 -0600

    Estructura de menú

--- main.py
+++ main.py
@@ -7,3 +7,29 @@ class Libro:
 
 books = []
 
+while True:
+    print('-----MENÚ-----')
+    print('1. Agregar libros.')
+    print('2. Mostrar lista de libros.')
+    print('3. Eliminar libros por título.')
+    print('4. Salir del programa.')
+
+    option = input('Ingrese una opción: ')
+    print()
+
+    match option:
+        case '1':
+            pass
+
+        case '2':
+            pass
+
+        case '3':
+            pass
+
+        case '4':
+            print('Saliendo del programa...')
+            break
+
+        case _:
+            print('Opción inválida. Intente nuevamente.\n')
